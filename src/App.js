0658d78d2382828027d104ab5af32e649e43e41d
huifang <hui-f.gu@hotmail.com>
2022-11-11 09:25:46 +1030

    background music

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import GameBoard from './components/GameBoard';
 import './App.css';
 import { Row } from 'react-bootstrap';
 import Quote from './components/Quote';
-
+import GOT from './video/GOT.mp3'
+import ReactAudioPlayer from 'react-audio-player';
 function App() {
   return (
     <div className="App">
+      <ReactAudioPlayer
+        src={GOT}
+        autoPlay
+        loop
+      />
       <Row className='text-center mt-2'>
         <h1 style={{fontFamily: "'Dancing Script', cursive"}}>Tic Tac Toe</h1>
         <Quote />
